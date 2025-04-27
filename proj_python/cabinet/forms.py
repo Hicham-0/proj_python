e7cbd740e81dad9d670293588cb55f119a414af7
Hicham-0 <mlyhichamelhanafi@gmail.com>
2025-04-27 11:52:12 +0100

    Add files via upload

---
+++ proj_python/cabinet/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from .models import Medecin, Patient
+
+
+
+class MedecinForm(forms.ModelForm):
+    class Meta:
+        model = Medecin
+        fields = ['nom', 'prenom', 'email', 'mot_de_passe', 'specialite', 'numero_telephone', 'ville']
+        widgets = {
+            'specialite': forms.Select(attrs={'class': 'form-select'}),
+            'ville': forms.Select(attrs={'class': 'form-select'}),
+        }
+
+
+class PatientForm(forms.ModelForm):
+    class Meta:
+        model = Patient
+        fields = ['nom', 'prenom', 'email', 'mot_de_passe', 'numero_telephone', 'date_naissance']
